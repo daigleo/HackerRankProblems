789bad8e8e0c7fff024ffeebec57ef82d036b968
Olivier Daigle <daigleo@gmail.com>
2019-06-24 08:00:27 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hacker Rank Problems
+This repository hosts Hacker Rank problems that I have attempted to solve.
---
+++ Sherlock and Squares.py
@@ -0,0 +1,14 @@
+''' 
+For this problem, we are given a range of integers and must return the number of perfect
+square within that range.
+
+constraints:
+1 <= q <= 100       The number of integers to check
+1 <= a <= b <= 10e9 The start and end of the range
+
+Link to problem:
+https://www.hackerrank.com/challenges/sherlock-and-squares/problem
+'''
+
+def square(a, b):
+    pass
